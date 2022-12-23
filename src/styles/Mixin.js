2d0ae5f91b5490d1a54e651966603d1ab834a64f
김민서 <kimminseo@192.168.nate.com>
2022-12-23 20:59:46 +0900

    Modify: package, styles폴더 수정

---
+++ src/styles/Mixin.js
@@ -0,0 +1,10 @@
+import { css } from 'styled-components';
+
+const Sticky = css`
+  position: fixed !important;
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.11);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.11);
+  transition: all 0.6s ease-in-out;
+  color: black;
+`;
