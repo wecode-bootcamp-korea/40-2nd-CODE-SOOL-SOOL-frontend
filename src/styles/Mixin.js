6cbe6bb7321948878f6205598054e1b6ea931885
김민서 <kimminseo@192.168.nate.com>
2022-12-23 21:22:29 +0900

    Modify: Mixin.js -> mixin.js

--- src/styles/Mixin.js
+++ src/styles/Mixin.js
@@ -1,7 +1,6 @@
 import { css } from 'styled-components';
 
 const Sticky = css`
-  position: fixed !important;
   background-color: white;
   border-bottom: 1px solid rgba(0, 0, 0, 0.11);
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.11);
