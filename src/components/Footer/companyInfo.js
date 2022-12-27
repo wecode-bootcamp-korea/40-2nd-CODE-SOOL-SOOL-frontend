e0be1f85fdd2178e944857271d1c726e5eb02589
yunakim <dbssla31@gmail.com>
2022-12-27 15:57:33 +0900

    Add: Footer 페이지 레이아웃 구현 및 상수데이터 폴더 생성 완료
    Add: Footer 레이아웃 구현, 상수데이터 폴더 생성(Data.js)
    
    Refactor: footer 레이아웃 구현
    - 상수데이터를 map 메서드를 이용하여 간결하게 코드 수정
    
    Modify: 파일명 수정 및 style 수정
    - footer mockdata 파일명 변경 (Data.js -> companyInfo.js)
    - FooterMainWrapper styled 변경 (width값 삭제)

---
+++ src/components/Footer/companyInfo.js
@@ -0,0 +1,48 @@
+export const COMPANYINFO_DATA = [
+  { id: 1, name: '대표 : 위코드' },
+  { id: 2, name: '사업자등록번호 : 123-45-67890' },
+  { id: 3, name: '사업자확인' },
+  { id: 4, name: '통신판매 신고번호 : 2022-코드술술-1234' },
+  {
+    id: 5,
+    name: '주소 : 서울특별시 강남구 테헤란로 427 위워크 타워(위워크 선릉 2호점)',
+  },
+  { id: 6, name: '정보보호 책임자 : 위코드' },
+  { id: 7, name: '대표 전화 : 123-4567-8910' },
+  { id: 8, name: '이메일 : codesoolsool@gmail.com' },
+];
+
+export const COMPANYINFO_ICON = [
+  {
+    id: 1,
+    src: 'https://cdn-icons-png.flaticon.com/512/1384/1384015.png',
+    alt: '인스타그램 아이콘',
+  },
+  {
+    id: 2,
+    src: 'https://cdn-icons-png.flaticon.com/512/4494/4494467.png',
+    alt: '유튜브 아이콘',
+  },
+  {
+    id: 3,
+    src: 'https://cdn-icons-png.flaticon.com/512/4902/4902370.png',
+    alt: '카카오톡 아이콘',
+  },
+  {
+    id: 4,
+    src: 'https://cdn-icons-png.flaticon.com/512/1384/1384005.png',
+    alt: '페이스북 아이콘',
+  },
+  {
+    id: 5,
+    src: 'https://cdn-icons-png.flaticon.com/512/2111/2111477.png',
+    alt: '구글 아이콘',
+  },
+];
+
+export const COMPANYINFO_MID_MENU = [
+  { id: 1, name: '이용약관' },
+  { id: 2, name: '개인정보방침' },
+  { id: 3, name: '입점문의' },
+  { id: 4, name: '기업 구매' },
+];
